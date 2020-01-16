070f427133de914b3630bd57126f8c1fa4089566
Raul A Rozo <raul.a.rozo.o@gmail.com>
2020-01-16 04:01:45 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,8 +22,8 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   array = [
-    DON_G
-    JOELLE_VD
+    DON_G,
+    JOELLE_VD,
     PAT_M,
     KATE_G,
     BRUCE_G
